c160b81e904f24bc57fd5d4976f1c4ff0f817d18
ABham2997 <azambham@hotmail.co.uk>
2019-08-12 00:01:22 +0100

    macro file added to create abstract unit type

---
+++ engunits/macro.h
@@ -0,0 +1,38 @@
+#ifndef __ENGUNITS_ABSTRACT_MACRO_H
+#define __ENGUNITS_ABSTRACT_MACRO_H
+
+#include "../engunits/_abstract/abstract_base.h"
+
+#define ADD_ABSTRACT_UNIT_TYPE(abstract_unit_name, si_unit_name)\
+namespace engunits::abstract_unit_name{\
+class si_unit_name;\
+\
+template<typename Child=si_unit_name>\
+class abstract_unit_name: public abstract::PhysicalUnit<abstract_unit_name, Child> {\
+    protected:\
+        using abstract::PhysicalUnit<abstract_unit_name, Child>::PhysicalUnit;\
+\
+    public:\
+        using SI_type = si_unit_name;\
+\
+        abstract_unit_name<Child>(const abstract_unit_name<Child> &other) : abstract::PhysicalUnit<abstract_unit_name, Child>{other.val} {};\
+        abstract_unit_name<Child>(abstract_unit_name<Child> &&other) : abstract::PhysicalUnit<abstract_unit_name, Child>{other.val} {};\
+        template <typename T>\
+        abstract_unit_name<Child>(const abstract_unit_name<T> &other) : abstract::PhysicalUnit<abstract_unit_name, Child>{(convert<Child>(other))} {};\
+        template<typename T>\
+        abstract_unit_name<Child>(abstract_unit_name<T> &&other) : abstract::PhysicalUnit<abstract_unit_name, Child>{(convert<Child>(other))} {};\
+\
+        virtual const std::string symbol() const = 0;\
+\
+        Child &operator=(double value) {this->val=value; return *static_cast<Child*>(this);}\
+        Child &operator=(const abstract_unit_name<Child> &other) { this->val = other.val; return *static_cast<Child*>(this);}\
+        Child &operator=(abstract_unit_name<Child> &&other) { this->val = other.val; return *static_cast<Child*>(this);}     \
+        template<typename T> \
+        Child &operator=(const abstract_unit_name<T> &other) { this->val = other.val; return *static_cast<Child*>(this);}\
+        template<typename T> \
+        Child &operator=(abstract_unit_name<T> &&other) { this->val = other.val; return *static_cast<Child*>(this);}\
+        \
+};\
+}\
+
+#endif
